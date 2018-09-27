b3cf380f54efd1bf821afc74427bd6e5777d227f
LuisSic <lantoni24@hotmail.com>
2018-09-26 18:32:31 -0600

    constantes, variables y funciones

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("HOLA Spartan");
+const x = 5;
+const y = 5;
+
+function multiplicacion(){
+    return x*y;
+}
+
+function FuncionParametos(a, b){
+    return a+b;
+}
+console.log(multiplicacion());
+console.log(FuncionParametos(1,1));
